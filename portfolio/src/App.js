ab2cde5a5a50917b8416107c7a110126fd31c251
Neilas <neilas.armonas@gmail.com>
2020-08-20 20:20:36 +0300

    Second commit

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
-  return (
+  return (    
     <div className="App">
+      <Navbar/>
       <header className="App-header">
         <p>
           Neilas Armonas
